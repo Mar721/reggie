7a1479fd4e0b4e14e5c661fe57f2ae628284d092
userName <1489117944@qq.com>
2022-10-27 11:48:53 +0800

    Initial commit

---
+++ src/main/java/com/example/reggie/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.reggie.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@Slf4j
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
